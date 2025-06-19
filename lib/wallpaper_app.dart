72ed87ca15692a6d293296a403e19eddbc3fee39
thu080son <thu080son@gmail.com>
2025-06-19 23:11:04 +0000

    Initial commit

---
+++ lib/wallpaper_app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wallpaper App',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wallpaper Gallery'),
+      ),
+      body: Center(
+        child: Text('Wallpaper Gallery'),
+      ),
+    );
+  }
+}
